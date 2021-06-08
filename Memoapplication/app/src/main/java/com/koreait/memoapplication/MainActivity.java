6930bff7b8321a2d63200ef8af699ca219f282a5
hyeon <9078807@naver.com>
2021-06-08 14:58:01 +0900

    2021-06-08

--- Memoapplication/app/src/main/java/com/koreait/memoapplication/MainActivity.java
+++ Memoapplication/app/src/main/java/com/koreait/memoapplication/MainActivity.java
@@ -1,10 +1,8 @@
-package com.koreait.myapplication;
+package com.koreait.memoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    // 페이지 이동 하는 방법.
-    public void clickLoginBtn(View v) {
-        Intent intent = new Intent(this,LoginActivity.class);
-
-        startActivity(intent);
     }
 }
